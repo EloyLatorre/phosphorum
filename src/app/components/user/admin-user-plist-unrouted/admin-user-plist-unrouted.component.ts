24d43eb090c942046b1c837ccf870a3c46d50e22
EloyLatorre <elatorrebriones@gmail.com>
2023-10-23 19:12:52 +0200

    Hecho supongo

--- src/app/components/user/admin-user-plist-unrouted/admin-user-plist-unrouted.component.ts
+++ src/app/components/user/admin-user-plist-unrouted/admin-user-plist-unrouted.component.ts
@@ -1,13 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
-
-
-interface PageEvent {
-  first: number;
-  rows: number;
-  page: number;
-  pageCount: number;
-}
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-admin-user-plist-unrouted',
@@ -15,36 +7,21 @@ interface PageEvent {
   styleUrls: ['./admin-user-plist-unrouted.component.css']
 })
 export class AdminUserPlistUnroutedComponent implements OnInit {
+  @ViewChild('modalConfirmacion') modalConfirmacion!: ElementRef;
 
   datos: any = [];
-
-
   first: number = 0;
   rows: number = 10;
   page: number = 0;
-
   orderField: string = "id";
   orderDirection: string = "asc";
+  confirmacionId: number = 0;
+  isModalVisible: boolean = false; // Agregamos una variable para controlar la visibilidad del modal
 
-  constructor(
-    private oHttpClient: HttpClient
-  ) { }
+  constructor(private oHttpClient: HttpClient) {}
 
   ngOnInit() {
     this.getPage();
-  }
-
-  getPage(): void {
-    this.oHttpClient.get("http://localhost:8083/user" + "?size=" + this.rows + "&page=" + this.page + "&sort=" + this.orderField + "," + this.orderDirection).subscribe({
-      next: (data: any) => {
-        this.datos = data;
-      },
-      error: (error: any) => {
-        this.datos = null;
-        console.log(error);
-      }
-
-    })
   }
 
   onPageChange(event: any) {
@@ -64,6 +41,47 @@ export class AdminUserPlistUnroutedComponent implements OnInit {
     this.getPage();
   }
 
+  mostrarModalConfirmacion(id: number) {
+    console.log('mostrarModalConfirmacion se está ejecutando con id:', id);
+    
+    this.confirmacionId = id;
+    this.isModalVisible = true;
+  }
+  
+  borrarUsuario(id: number) {
+    if (id !== 0) {
+      this.oHttpClient.delete(`http://localhost:8083/user/${id}`).subscribe({
+        next: () => {
+          this.getPage();
+          console.log('La función borrarUsuario se ha ejecutado');
+          this.isModalVisible = false;
+        },
+        error: (error: any) => {
+          console.log('La función borrarUsuario no va');
+          console.error('Error al eliminar usuario:', error);
+          
+        }
+      });
+    }
+  }
+  
+  
+  
+  
 
+  ocultarModalConfirmacion() {
+    this.isModalVisible = false;
+  }
 
+  getPage(): void {
+    this.oHttpClient.get(`http://localhost:8083/user?size=${this.rows}&page=${this.page}&sort=${this.orderField},${this.orderDirection}`).subscribe({
+      next: (data: any) => {
+        this.datos = data;
+      },
+      error: (error: any) => {
+        this.datos = null;
+        console.log(error);
+      }
+    });
+  }
 }
